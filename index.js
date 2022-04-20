c09027af504205cd494e7551ea8bed4b7e83b600
Igor <ogrik29@gmail.co>
2022-04-20 19:36:03 +0300

    added registration form

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import { login } from "./src/pages/login/login"
+import { login } from "./src/pages/authorization/login/login"
+import { registration } from "./src/pages/authorization/registration/registration";
 
 import './index.scss'
 
 const RenderDom = () => {
     const root = document.getElementById('root')
 
-    root.innerHTML = login()
+    root.innerHTML = registration()
 }
 
 RenderDom();
---
+++ src/pages/authorization/registration/registration.js
@@ -0,0 +1,29 @@
+import Handlebars from 'handlebars'
+
+import tmpl from './registration.hbs'
+import {authorizationForm} from "../../../components/authorizationForm/authorizationForm";
+import './registration.scss';
+
+const data = {
+    formName: 'registrationForm',
+    title: 'Регистрация',
+    fields: [
+        {label: "Почта", type: 'text',  name: 'email'},
+        {label: "Логин", type: 'text',  name: 'login'},
+        {label: "Имя", type: 'text',  name: 'first_name'},
+        {label: "Фамилия", type: 'text',  name: 'second_name'},
+        {label: "Телефон", type: 'text', name: 'phone'},
+        {label: "Пароль", type: 'password', name: 'password'},
+        {label: "Пароль (еще раз)", type: 'password', name: 'passwordRepeat'}
+    ],
+    buttons: [
+        {type: 'submit', text: 'Зарегистрироваться', form: 'registrationForm'},
+        {type: 'button', text: 'Войти', form: 'registrationForm'}
+    ]
+}
+
+export const registration = () => {
+    Handlebars.registerPartial('loginForm', authorizationForm(data))
+
+    return tmpl()
+}
