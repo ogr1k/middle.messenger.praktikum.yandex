b15b5854cdf360e3dcae43e3f058c44252ac74cf
Igor <ogrik29@gmail.co>
2022-04-22 11:04:21 +0300

    change interimRouting

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
-import Handlebars from "handlebars";
+import Handlebars from "handlebars"
 
-import {login, registration, chats, profile, internalError, wrongPathError} from './src/pages'
 import {interimNavigationButtons} from "./src/components/interimNavigationButtons/interimNavigationButtons"
-import {ProfilePageTypes} from "./src/pages/profile/profile";
 import tmpl from './index.hbs'
 import './index.scss'
-
-Handlebars.registerPartial('header' , interimNavigationButtons())
-Handlebars.registerPartial('page' , login())
-
-const addEventListenersToInterimNav = (root) => {
-    const navButtons = root.querySelector('.interimNavigationButtons').querySelectorAll('button')
-    navButtons.forEach((el) => {
-        el.addEventListener('click',() => {
-            switch (el.id) {
-                case 'loginNav': {
-                    Handlebars.registerPartial('page' , login())
-                } break;
-                case 'registrationNav': {
-                    Handlebars.registerPartial('page' , registration())
-                } break;
-                case 'chatNav': {
-                    Handlebars.registerPartial('page' , chats())
-                } break;
-                case 'profileNav': {
-                    Handlebars.registerPartial('page' , profile(ProfilePageTypes.PROFILE))
-                } break;
-                case 'profileEditNav': {
-                    Handlebars.registerPartial('page' , profile(ProfilePageTypes.PROFILE_EDIT))
-                } break;
-                case 'profileEditPassNav': {
-                    Handlebars.registerPartial('page' , profile(ProfilePageTypes.PROFILE_CHANGE_PASSWORD))
-                } break;
-                case 'internalErrorNav': {
-                    Handlebars.registerPartial('page' , internalError())
-                } break;
-                case 'wrongPathNav': {
-                    Handlebars.registerPartial('page' , wrongPathError())
-                } break;
-            }
-            root.innerHTML = tmpl()
-            addEventListenersToInterimNav(root)
-        })
-    })}
+import {addEventListenersToInterimNav, registerPagePartialDependsOnCurrentPath} from "./src/utils/interimRouting"
 
 const RenderDom = () => {
     const root = document.getElementById('root')
+    Handlebars.registerPartial('header' , interimNavigationButtons())
+    registerPagePartialDependsOnCurrentPath();
 
     root.innerHTML = tmpl()
     addEventListenersToInterimNav(root)
---
+++ src/utils/contants/paths.js
@@ -0,0 +1,10 @@
+export const Paths = {
+    LOGIN: '/login',
+    REGISTRATION: '/registration',
+    CHAT: '/chat',
+    PROFILE: '/profile',
+    PROFILE_EDIT_INTERIM: '/profileEdit',
+    PROFILE_EDIT_PASSWORD_INTERIM: '/profileEditPassword',
+    INTERNAL: '/internal',
+    WRONG_PATH: '/wrong_path_test'
+}
